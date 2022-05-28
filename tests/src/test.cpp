e13cbff42a1a5c479ed6d77f1113344bddfea8dd
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 21:51:24 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,25 +1,6 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/osx/iokit_iterator.hpp>
 #include <pqrs/osx/iokit_types.hpp>
-
-TEST_CASE("iokit_iterator") {
-  {
-    pqrs::osx::iokit_iterator iokit_iterator;
-    REQUIRE(iokit_iterator.valid() == false);
-  }
-
-  {
-    auto entry = IORegistryGetRootEntry(type_safe::get(pqrs::osx::iokit_mach_port::null));
-    io_iterator_t it = IO_OBJECT_NULL;
-    pqrs::osx::kern_return r = IORegistryEntryGetChildIterator(entry, kIOServicePlane, &it);
-    REQUIRE(static_cast<bool>(r) == true);
-
-    pqrs::osx::iokit_iterator iokit_iterator(it);
-    REQUIRE(iokit_iterator.valid() == true);
-  }
-}
 
 namespace {
 pqrs::osx::iokit_iterator copy_constructor_test(void) {
@@ -28,6 +9,30 @@ pqrs::osx::iokit_iterator copy_constructor_test(void) {
 }
 } // namespace
 
-TEST_CASE("iokit_iterator::copy_constructor_test") {
-  copy_constructor_test();
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "iokit_iterator"_test = [] {
+    {
+      pqrs::osx::iokit_iterator iokit_iterator;
+      expect(iokit_iterator.valid() == false);
+    }
+
+    {
+      auto entry = IORegistryGetRootEntry(type_safe::get(pqrs::osx::iokit_mach_port::null));
+      io_iterator_t it = IO_OBJECT_NULL;
+      pqrs::osx::kern_return r = IORegistryEntryGetChildIterator(entry, kIOServicePlane, &it);
+      expect(static_cast<bool>(r) == true);
+
+      pqrs::osx::iokit_iterator iokit_iterator(it);
+      expect(iokit_iterator.valid() == true);
+    }
+  };
+
+  "iokit_iterator::copy_constructor_test"_test = [] {
+    copy_constructor_test();
+  };
+
+  return 0;
 }
