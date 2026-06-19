c60c40d25e10889f07b03b76c2626b6a9379f4e0
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-19 21:28:56 +0900

    v1.2.0

--- include/pqrs/osx/iokit_iterator.hpp
+++ include/pqrs/osx/iokit_iterator.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_iterator v1.1
+// pqrs::osx::iokit_iterator v1.2.0
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
